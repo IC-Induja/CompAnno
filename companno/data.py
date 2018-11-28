fadfe4dd3a284487f5b6253b80320ae7a8ab0498
IC-Induja <ic.induja@gmail.com>
2018-11-28 16:58:33 -0500

    Fixed typos.

--- companno/data.py
+++ companno/data.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
 
-cogs_path = os.path.join(os.path.dirname(__file__), , 'cognames.tsv')
+cogs_path = os.path.join(os.path.dirname(__file__), 'data', 'cognames.tsv')
 COGS = pd.read_table(cogs_path, sep="\t", comment="#", index_col=0,
     names=["COG", "function", "name"])
